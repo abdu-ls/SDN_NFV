44451bd149330a720c252605bedf1d811a0c8540
abdu-ls <111575612+abdu-ls@users.noreply.github.com>
2025-10-05 06:57:34 +0000

    Update lightpath.py

--- lightpath.py
+++ lightpath.py
@@ -1,7 +1,7 @@
 
 """
 1. RUn floodlight
-2. Run a topology on mininet: 
+2. Run a topology on mininet: sudo mn --topo linear,3 --controller=remote,ip=127.0.0.1,port=6653 --switch ovsk,protocols=OpenFlow10
 3. Implement the lightpath.py: python3 lightpath.py --src h1 --dst h3 --floodlight http://127.0.0.1:8080 --bidirectional
 4. Show installation using floodlight REST API: curl http://127.0.0.1:8080/wm/core/switch/all/flow/json | jq
 
