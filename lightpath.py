1bd6476218a965bd64c68cc500358b390f35f1a4
abdu-ls <111575612+abdu-ls@users.noreply.github.com>
2025-10-05 06:56:34 +0000

    Update lightpath.py

--- lightpath.py
+++ lightpath.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python3
-"""
-lightweight_fixed.py
 
-Improved/robust version of your original script to handle Floodlight API JSON variations.
+"""
+1. RUn floodlight
+2. Run a topology on mininet: 
+3. Implement the lightpath.py: python3 lightpath.py --src h1 --dst h3 --floodlight http://127.0.0.1:8080 --bidirectional
+4. Show installation using floodlight REST API: curl http://127.0.0.1:8080/wm/core/switch/all/flow/json | jq
+
+5. Show individual switches on mininet:
+    sh ovs-ofctl dump-flows s1
+    sh ovs-ofctl dump-flows s2
+    sh ovs-ofctl dump-flows s3
+
+ping test on mininet: h1 ping h3
 """
 
 import requests
